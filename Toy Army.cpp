427b49d230da1e4dd063078e544b93a45d433754
ALTOHA17 (JEI) <altoha17@hotmail.com>
2017-08-30 08:02:34 -0500

    Add files via upload

---
+++ Toy Army.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	int soldados;
+
+	scanf("%d", &soldados);
+
+	printf ("%d\n",(soldados/2)*3);
+	system("pause");
+return 0;
+}
